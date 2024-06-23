88e4f2aded2e29487e8f0295b4c2b757d3b54aee
RadeStojicic <radestojicicsd@gmail.com>
2024-06-23 15:44:27 +0200

    weather-app

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vremenska prognoza
+
+## Kloniranje projekta
+
+Pokrenuti komandu za kloniranje:
+
+```bash
+git clone https://github.com/elab-development/klijentske-veb-tehnologije-2024-2022-0251-vremenska-prognoza.git .
+```
+
+Nakon toga, instalirati dependencies uz pomoc vaseg omiljenog package managera:
+
+```bash
+pnpm i
+```
+
+I pokrenite dev server:
+
+```bash
+pnpm dev
+```
---
+++ test/Button.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { describe, expect, test } from "vitest";
+import { Button } from "../src/components/ui/button";
+
+describe("Button", () => {
+  test("Button renders successfully", () => {
+    render(<Button>ButtonText</Button>);
+    expect(screen.getByText("ButtonText")).toBeDefined();
+  });
+});
